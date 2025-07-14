5a9356ddabee6546e3408a4352ebf4aede5bbceb
ruran8wa <p.rurangwa@alustudent.com>
2025-07-14 11:06:23 +0200

    fixed the api internal error issue

--- app/schemas.py
+++ app/schemas.py
@@ -2,21 +2,21 @@ from pydantic import BaseModel
 
 class PatientBase(BaseModel):
     name: str
-    Age: int
-    Sex: str
-    RestingBP: float
-    Cholesterol: float
-    FastingBS: float
-    MaxHR: float
-    ExerciseAngina: float
-    Oldpeak: float
-    HeartDisease: float
-    ST_Slope_encoded: float
-    RestingECG_encoded: float
-    ChestPain_ASY: float
-    ChestPain_ATA: float
-    ChestPain_NAP: float
-    ChestPain_TA: float
+    age: int
+    sex: str
+    resting_bp: float
+    cholesterol: float
+    fasting_bs: float
+    max_hr: float
+    exercise_angina: float
+    oldpeak: float
+    heart_disease: float
+    st_slope_encoded: float
+    resting_ecg_encoded: float
+    chest_pain_asy: float
+    chest_pain_ata: float
+    chest_pain_nap: float
+    chest_pain_ta: float
 
 class PatientCreate(PatientBase):
     pass
